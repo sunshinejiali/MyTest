680569240fa53fcdc34a8eed5b7b1626ce6e9144
sjl <sunshinejiali98@163.com>
2021-05-19 10:51:51 +0800

    init

---
+++ helloworld/build/CMakeFiles/greeter_async_client3.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "helloworld.pb.cc"
+  "helloworld.pb.h"
+  "helloworld.grpc.pb.cc"
+  "helloworld.grpc.pb.h"
+  "CMakeFiles/greeter_async_client3.dir/greeter_async_client3.cc.o"
+  "CMakeFiles/greeter_async_client3.dir/helloworld.pb.cc.o"
+  "CMakeFiles/greeter_async_client3.dir/helloworld.grpc.pb.cc.o"
+  "greeter_async_client3.pdb"
+  "greeter_async_client3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greeter_async_client3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ helloworld/cmake-build-debug/CMakeFiles/greeter_async_client3.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greeter_async_client3.dir/greeter_async_client3.cc.o"
+  "CMakeFiles/greeter_async_client3.dir/helloworld.grpc.pb.cc.o"
+  "CMakeFiles/greeter_async_client3.dir/helloworld.pb.cc.o"
+  "greeter_async_client3"
+  "greeter_async_client3.pdb"
+  "helloworld.grpc.pb.cc"
+  "helloworld.grpc.pb.h"
+  "helloworld.pb.cc"
+  "helloworld.pb.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greeter_async_client3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
